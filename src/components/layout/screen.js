265a42dcf572caadee4d013af5245792a85e5d1f
TomAnderson94 <137895998+TomAnderson94@users.noreply.github.com>
2024-01-12 16:33:45 +0000

    Module List Interactable

--- src/components/layout/screen.js
+++ src/components/layout/screen.js
@@ -21,8 +21,6 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
 
--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,25 +1,47 @@
-import { StyleSheet, Text,  } from 'react-native';
+import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 import Screen from '../layout/screen';
+import initialModules from '../../data/modules.js';
 
 export const ModuleListScreen = () => {
+
   // Initialisation ------------------------------
+  const modules = initialModules;
 
   // State ------------------------------
 
   // Handlers ------------------------------
+  const handleSelect = () => alert("Hi! Item Selected!")
 
   // View ------------------------------
   return (
     <Screen>
-
-
-      <Text>List</Text>
-
-
+      <ScrollView style={styles.container}>
+        {modules.map((module) =>{
+          return(
+            <Pressable key={module.ModuleCode} onPress={handleSelect}>
+            <View style={styles.item}>
+              <Text style={styles.text}> 
+              {module.ModuleCode} {module.ModuleName}
+              </Text>
+          </View>
+          </Pressable>
+        );
+      })}
+      </ScrollView>
     </Screen>
   );
-}
+};
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+  item: {
+    paddingVertical: 15,
+    borderTopWidth: 1,
+    borderColor: 'lightgray',
+  },
+  text: {
+    FontSize: 16,
+  },
+});
 
 export default ModuleListScreen;
