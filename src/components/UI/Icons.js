8ce7d23bcce017793f8b4ce4b624775f2f270353
TomAnderson94 <137895998+TomAnderson94@users.noreply.github.com>
2024-01-22 14:37:03 +0000

    Fixing button

--- src/components/UI/Icons.js
+++ src/components/UI/Icons.js
@@ -2,9 +2,9 @@ import { MaterialIcons } from '@expo/vector-icons';
 
 const Icons = {};
 
-const Add = () => {<MaterialIcons name='add' size={16}/>};
-const Edit = () => {<MaterialIcons name='edit' size={16}/>};
-const Delete = () => {<MaterialIcons name='delete' size={16}/>};
+const Add = () => <MaterialIcons name='add' size={16}/>;
+const Edit = () => <MaterialIcons name='edit' size={16}/>;
+const Delete = () => <MaterialIcons name='delete' size={16}/>;
 
 // Compose 
 Icons.Add = Add;
