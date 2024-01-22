96bd76be25536a254fc1722e0426117b7d0205d6
TomAnderson94 <137895998+TomAnderson94@users.noreply.github.com>
2024-01-22 15:43:52 +0000

    Creating an add form

--- src/components/UI/Icons.js
+++ src/components/UI/Icons.js
@@ -5,10 +5,13 @@ const Icons = {};
 const Add = () => <MaterialIcons name='add' size={16}/>;
 const Edit = () => <MaterialIcons name='edit' size={16}/>;
 const Delete = () => <MaterialIcons name='delete' size={16}/>;
+const Close = () => <MaterialIcons name ='close' size={16}/>;
 
 // Compose 
 Icons.Add = Add;
 Icons.Edit = Edit;
 Icons.Delete = Delete;
+Icons.Close = Close;
+
 
 export default Icons;
